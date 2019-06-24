1d9ad91ecbd6677e726fe36d625a3065b4955693
Enayatullah Naseri <ena.nas@icloud.com>
2019-06-24 12:54:43 -0700

    Started the star project

---
+++ StarRating/StarRating/ViewControllers/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  StarRating
+//
+//  Created by Enayatullah Naseri on 6/24/19.
+//  Copyright © 2019 Enayatullah Naseri. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
